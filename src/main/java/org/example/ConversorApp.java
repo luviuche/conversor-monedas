a49c5b0d469ebf787b37c74e18e9fb7b8d9dc48f
lmvm <lmvm.zzz@gmail.com>
2026-02-19 11:13:54 -0500

    Añade el HttpClient, HttpRequest y HttpResponse

--- src/main/java/org/example/ConversorApp.java
+++ src/main/java/org/example/ConversorApp.java
@@ -1,7 +1,33 @@
 package org.example;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConversorApp {
     static void main() {
-        System.out.println("Aqui empieza el proyecto!!");
+        // 1. Define la URL exacta que usaste en Postman (reemplaza TU_CLAVE_API)
+        String direccion = "https://v6.exchangerate-api.com/v6/f0c8c87f20abd145afc0f84e/latest/USD";
+
+        try {
+            // 2. Construimos el Cliente (El Mensajero)
+            HttpClient client = HttpClient.newHttpClient();
+
+            // 3. Construimos la Solicitud (La Carta)
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
+
+            // 4. Enviamos la solicitud y guardamos la Respuesta (El Paquete)
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            // 5. Imprimimos el cuerpo de la respuesta en la consola
+            System.out.println("Respuesta de la API:");
+            System.out.println(response.body());
+
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al realizar la petición: " + e.getMessage());
+        }
     }
 }
